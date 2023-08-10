3e7d14304d9b3bf5056e3a1fbddd593f5abb1176
sabbir <sabbir@gmail.com>
2023-08-10 17:25:11 +0600

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import Navbar from "../components/Navbar";
+
+const Home: React.FC = () => {
+  return <div></div>;
+};
+
+export default Home;
---
+++ src/routes/router.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import App from "../App";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
+
+export default router;
